8caf2a3312dd9c44812c05fc29d183e514c3a106
YOO TAEYANG <yty0706@gmail.com>
2024-04-05 11:53:54 +0900

    0405 스터디 파일 추가

---
+++ 0405-Study/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _0405_Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("######## 4.9");
+            // &
+            // |
+            // ^
+            // ~
+
+            int x = 10;
+            // 0b1010
+
+            int y = 9;
+            // 0b1001
+
+            Console.WriteLine(x & y);
+
+            //0b1010
+            //0b1001
+            //0b1000 -> 8
+
+            Console.WriteLine(x | y);
+
+            //0b1010
+            //0b1001
+            //0b1011
+
+            Console.WriteLine(x ^ y);
+
+            //0b1010
+            //0b1001
+            //0b0011
+
+            Console.WriteLine("######## 4.10");
+
+            int? s = null;
+            
+            Console.WriteLine(s ?? 100);
+
+            Console.WriteLine("######## 문제");
+
+            // 1
+
+            //int a1 = 12;
+            //int b1 = 6;
+
+            // b1 = 0b0110
+            // a1 = 0b1100
+            // a | b = 0b1110 = 14
+
+            // 2
+
+            // b = 14
+            // a = 12
+
+            // -> a = -2
+
+            // 3
+
+            // a = 14 / -1
+            // a = -14
+
+            // a = -14
+            // b = 14
+
+            Console.WriteLine("######## 5.1");
+
+        }
+    }
+}
---
+++ 4.8.0_NullConditionalOperator/4.8.0_NullConditionalOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace _4._8._0_NullConditionalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구"); // a 자체가 null을 반환하기 때문에 뒤에 메소드 사용 불가
+            a?.Add("축구");
+            Console.WriteLine($"Count : {a?.Count}");
+            Console.WriteLine($"{a?[0]}");
+            Console.WriteLine($"{a?[1]}");
+
+            a = new ArrayList();
+            a?.Add("야구");
+            Console.WriteLine($"Count : {a?.Count}");
+        }
+    }
+}
