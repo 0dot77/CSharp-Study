4bdfaebda71b0d6755afaf1be43e91897f22274d
0dot77 <yty0706@gmail.com>
2024-04-12 12:31:04 +0900

    0412 스터디 준비

---
+++ 5.3_Jump_Statement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _5._3_Jump_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // #### Break ####
+
+            while (true)
+            {
+                Console.Write("계속할까요?(예/아니오) : ");
+                String answer = Console.ReadLine();
+
+                if (answer == "아니오")
+                    break;
+            }
+            */
+
+            /*
+            // #### continue ####
+
+            for (int i = 0; i < 4; i++)
+            {
+                if(i == 2)
+                {
+                    Console.WriteLine("여기는 건너 뛸 것임");
+                    continue;
+                } else
+                {
+                    Console.WriteLine("Attack! ⚔️");
+                }
+            }
+            */
+        }
+    }
+}
