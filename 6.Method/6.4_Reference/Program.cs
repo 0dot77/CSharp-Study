c1a700d5cb143033f647403b0cb4ad9c64b0fb6e
YOO TAEYANG <yty0706@gmail.com>
2024-04-17 15:57:09 +0900

    0417 폴더 정리

---
+++ 6.Method/6.4_Reference/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _6._4_Reference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+}
