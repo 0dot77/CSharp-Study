cede7a31dd00caf68e0e74d6f6513bfc2efff412
0dot77 <yty0706@gmail.com>
2024-05-10 00:27:50 +0900

    0510 스터디 준비 마무리

---
+++ 08.004_Quiz/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _08._004_Quiz
+{
+    interface IRunnable
+    {
+        void Run();
+    }
+
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    class Car : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run Run! by Car");
+        }
+    }
+
+    class Plane : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Fly! by Plane");
+        }
+    }
+
+
+    class FlyingCar : IRunnable, IFlyable
+    {
+        private Car c = new Car();
+        private Plane p = new Plane();
+        public void Run()
+        {
+            c.Run();
+            Console.WriteLine("Run! Run!");
+        }
+
+        public void Fly()
+        {
+            p.Fly();
+            Console
+                .WriteLine("Fly! FlY");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlyingCar car = new FlyingCar();
+            car.Run();
+            car.Fly();
+        }
+    }
+}
